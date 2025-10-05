34bb9e875f0598b8006b5099e0f1654052c98d5a
franciscoabsl <franciscosouzalima@gmail.com>
2025-10-05 00:23:49 -0300

    add SQL scripts for consultas and medicos_projection; include Postman collection for API testing

---
+++ docs/dados/medicosprojection.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.medicos_projection
+(id, especialidade, nome, numero_registro, "role")
+VALUES('668dca87-262e-4136-a58d-d3299188165a'::uuid, 'CARDIOLOGIA', 'Carlos Oliveira', 'CRM/PE-12345', 'MEDICO');
+INSERT INTO public.medicos_projection
+(id, especialidade, nome, numero_registro, "role")
+VALUES('dd5c5f72-46cf-4f1e-903f-fe9fffb6c8f8'::uuid, 'ORTOPEDIA', 'Elisa Santos', 'CRM/SP-67890', 'MEDICO');
+INSERT INTO public.medicos_projection
+(id, especialidade, nome, numero_registro, "role")
+VALUES('390e7edb-2b9f-49ab-ac1d-6b783051ce37'::uuid, 'CLÍNICO GERAL', 'Fernando Costa', 'CRM/SC-54321', 'MEDICO');
